0b422b19e9fac387f0f13ad0f297dd807fefa76e
Tu Nguyen <tuna9x.it@gmail.com>
2025-02-21 16:47:56 +0700

    add domain2

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import TodosContextProvider from "./contexts/TodosContextProvider.tsx";
-const URL_PROD = "https://todo-auth-mu.vercel.app/";
+const URL_PROD = "https://todo-auth-mu.vercel.app";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
