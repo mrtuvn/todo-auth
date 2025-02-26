a919a9f112ff51cbc2b5beb7cd1e3bc03e5cfdae
Tu Nguyen <tuna9x.it@gmail.com>
2025-02-26 22:18:28 +0700

    Fix broken link image

--- dist/assets/NoPage-DuSf6kV3.js
+++
@@ -1,1 +0,0 @@
-import{j as e}from"./index-Cg9j8mSu.js";const r=()=>e.jsx("div",{children:"NoPage"});export{r as default};
--- src/components/OldMemo.tsx
+++ src/components/OldMemo.tsx
@@ -7,32 +7,32 @@ export function OldMemo() {
     {
       title: "Chúng ta đã có một trải nghiệm tuyệt vời cùng nhau",
       button: "Explore Component",
-      src: "/public/memory/group0.jpg",
+      src: "/memory/group0.jpg",
     },
     {
       title: "Chụp ảnh cái nào",
       button: "Explore Component",
-      src: "/public/memory/group1.jpg",
+      src: "/memory/group1.jpg",
     },
     {
       title: "Tiếp tục chụp tiếp nào",
       button: "Explore Component",
-      src: "/public/memory/group2.jpg",
+      src: "/memory/group2.jpg",
     },
     {
       title: "Chụp nhanh đi chứ thèm quá rồi nè",
       button: "Explore Component",
-      src: "/public/memory/ha.jpg",
+      src: "/memory/ha.jpg",
     },
     {
       title: "Mị đói rồi ăn thôi",
       button: "Explore Component",
-      src: "/public/memory/ha2.jpg",
+      src: "/memory/ha2.jpg",
     },
     {
       title: "Em tôi quá vui sau khi được cắt bánh",
       button: "Explore Component",
-      src: "/public/memory/thao.jpg",
+      src: "/memory/thao.jpg",
     },
   ];
 
